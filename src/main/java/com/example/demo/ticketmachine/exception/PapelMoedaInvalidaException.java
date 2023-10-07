cf9c2bfbf095c9c29928cb57720bf017cccac7fb
PEDRO HENRIQUE G SANCHEZ <uniepsanchez>
2023-10-06 21:47:20 -0300

    Add TicketMachine

---
+++ src/main/java/com/example/demo/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -0,0 +1,9 @@
+package main.java.com.demo.ticketmachine.exception;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoedaInvalidaException extends Exception {
+    
+}
---
+++ src/main/java/com/example/demo/ticketmachine/exception/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package main.java.com.demo.ticketmachine.exception;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class SaldoInsuficienteException extends Exception {
+}
