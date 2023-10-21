cd253ad86b67dfc1d9b822c47cad64c026c10a1f
Gvelecico <gvelecico@hotmail.com>
2023-10-21 00:55:48 -0300

    TicketMachineTests

--- src/test/java/com/example/demo/TicketMachineTests.java
+++ src/test/java/com/example/demo/TicketMachineTests.java
@@ -1,11 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.ticketmachine.core.TicketMachine;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TicketMachineTests {
 
 	@Test
-	void contextLoads() {
+	void insertCorrectTest() throws main.java.com.demo.ticketmachine.exception.PapelMoedaInvalidaException {
+
+		TicketMachine ticketMachine = new TicketMachine(10);
+		ticketMachine.inserir(2);
+		ticketMachine.inserir(2);
+		assertEquals(4, ticketMachine.getSaldo());
+	}
+
+	@Test
+	void insertNotCorrectTest() {
+
+		TicketMachine ticketMachine = new TicketMachine(10);
+
+		assertThrows(main.java.com.demo.ticketmachine.exception.PapelMoedaInvalidaException.class,
+				() -> {
+					ticketMachine.inserir(7);
+				});
 	}
 
 }
